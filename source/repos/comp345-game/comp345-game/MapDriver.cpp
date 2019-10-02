19f70342e58928d642f28f9e1ecd0326eb6da105
Charles Rabbat <chuckthedunker@gmail.com>
2019-10-01 20:55:25 -0400

    renamed files to required conventions of the assignment documents. Replaced structs with classes.

--- source/repos/comp345-game/comp345-game/MapDriver.cpp
+++ source/repos/comp345-game/comp345-game/MapDriver.cpp
@@ -3,15 +3,17 @@
 
 #include "pch.h"
 #include <iostream>
-#include "MapBuilder.h"
+#include "Map.h"
 
 using namespace std;
 
 int main()
 {
 	int totalVertices = 4;
-	MapBuilder newMap;
-	MapBuilder::Graph *graph;
+	// MapBuilder newMap = MapBuilder(5);
+
+	Map::MapGraph *graph;
+	Map newMap;
 
 	graph = newMap.createGraph(totalVertices);
 
@@ -21,7 +23,15 @@ int main()
 	newMap.addEdge(graph, 1, 3);
 	newMap.addEdge(graph, 2, 3);
 
-    cout << "Hello World!" << endl; 
+	//graph = createGraph(totalVertices);
+
+	//graph.addEdge(graph, 0, 1);
+	//newMap.addEdge(graph, 0, 2);
+	//newMap.addEdge(graph, 0, 3);
+	//newMap.addEdge(graph, 1, 3);
+	//newMap.addEdge(graph, 2, 3);
+
+ //   cout << "Hello World!" << endl; 
 		
 	newMap.printGraph(graph);
 }
